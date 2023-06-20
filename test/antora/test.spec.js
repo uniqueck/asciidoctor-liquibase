76677f8bb22ed80aaf39efb335ff5f1e29235e80
Constantin Krüger <Constantinidis@web.de>
2023-06-20 23:36:56 +0200

    Chore: Fix lint errors & warnings

--- test/antora/test.spec.js
+++ test/antora/test.spec.js
@@ -1,16 +1,19 @@
-/* global describe it beforeEach */
+/* global describe it before */
 const rimrafSync = require('rimraf')
 const generateSite = require('@antora/site-generator-default')
+const path = require('path')
 
 describe('Antora integration', function () {
-    this.timeout(10000)
-    before(async function () {
-        rimrafSync(`${__dirname}/public`, function (error) {})
-        await generateSite([`--playbook=${__dirname}/antora-playbook.yml`])
+  this.timeout(10000)
+
+  before(async function () {
+    /* eslint-disable n/handle-callback-err */
+    rimrafSync(path.join(__dirname, 'public'), function (error) {
     })
+    await generateSite([`--playbook=${path.join(__dirname, 'antora-playbook.yml')}`])
+  })
 
-    it('blockmacro', async () => {
+  it('blockmacro', async () => {
 
-    })
-
+  })
 })

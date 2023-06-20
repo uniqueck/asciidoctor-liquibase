86cf4119b872b301b4cb67562d1a0dc11eb7d568
Constantin Krüger <Constantinidis@web.de>
2023-06-20 23:36:56 +0200

    Chore: Fix lint errors & warnings

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,35 +1,36 @@
-/* global describe it before */
+/* global Opal describe it before */
 const asciidoctorLiquibase = require('../lib/asciidoctor-liquibase.js')
 const asciidoctor = require('@asciidoctor/core')()
 
-const { readFixture, fixturePath, deleteDirWithFiles } = require('./utils.js')
+const { fixturePath } = require('./utils.js')
 
 const chai = require('chai')
 const expect = chai.expect
 
 describe('Registration', () => {
-    it('should register the extention', () => {
-        const registry = asciidoctor.Extensions.create()
-        expect(registry['$block_macros?']()).to.be.false
-        asciidoctorLiquibase.register(registry)
-        expect(registry['$block_macros?']()).to.be.true
-        expect(registry['$registered_for_block_macro?']('liquibase')).to.be.an('object')
-    })
+  it('should register the extention', () => {
+    const registry = asciidoctor.Extensions.create()
+    /* eslint-disable no-unused-expressions */
+    expect(registry['$block_macros?']()).to.be.false
+    asciidoctorLiquibase.register(registry)
+    /* eslint-disable no-unused-expressions */
+    expect(registry['$block_macros?']()).to.be.true
+    expect(registry['$registered_for_block_macro?']('liquibase')).to.be.an('object')
+  })
 })
 
 describe('Conversion', () => {
-    before(() => {
-        Opal.Asciidoctor.LoggerManager.getLogger().setLevel(1)
-    })
+  before(() => {
+    Opal.Asciidoctor.LoggerManager.getLogger().setLevel(1)
+  })
 
-
-    describe('When extension is registered', () => {
-      it('should handle addModifyColumn correct', () => {
-          const file = fixturePath('liquibase', 'modifyDataType.yaml')
-          const registry = asciidoctor.Extensions.create()
-          asciidoctorLiquibase.register(registry)
-          const html = asciidoctor.convert(`liquibase::${file}[]`, { extension_registry: registry })
-          expect(html).to.contain(`<pre>'hide the spot
+  describe('When extension is registered', () => {
+    it('should handle addModifyColumn correct', () => {
+      const file = fixturePath('liquibase', 'modifyDataType.yaml')
+      const registry = asciidoctor.Extensions.create()
+      asciidoctorLiquibase.register(registry)
+      const html = asciidoctor.convert(`liquibase::${file}[]`, { extension_registry: registry })
+      expect(html).to.contain(`<pre>'hide the spot
 hide circle
 skinparam tabSize 4
 ' avoid problems with angled crows feet
@@ -40,15 +41,14 @@ entity person {
 \\t\\t\\tid:int
 }
 'relationships</pre>`)
+    })
 
-      })
-
-        it('should handle addColumn correct', () => {
-            const file = fixturePath('liquibase', 'addColumn.yaml')
-            const registry = asciidoctor.Extensions.create()
-            asciidoctorLiquibase.register(registry)
-            const html = asciidoctor.convert(`liquibase::${file}[]`, { extension_registry: registry })
-            expect(html).to.contain(`<pre>'hide the spot
+    it('should handle addColumn correct', () => {
+      const file = fixturePath('liquibase', 'addColumn.yaml')
+      const registry = asciidoctor.Extensions.create()
+      asciidoctorLiquibase.register(registry)
+      const html = asciidoctor.convert(`liquibase::${file}[]`, { extension_registry: registry })
+      expect(html).to.contain(`<pre>'hide the spot
 hide circle
 skinparam tabSize 4
 ' avoid problems with angled crows feet
@@ -60,9 +60,6 @@ entity person {
 \\t\\t\\tname:varchar
 }
 'relationships</pre>`)
-        })
-
-
     })
-
+  })
 })
